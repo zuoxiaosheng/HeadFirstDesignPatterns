3168b9c17cce6688a900801ed41cd4ee3f0e509e
unknown <Crayon@NBCSRZXS1.cddev.ccb.cn>
2014-05-13 22:40:25 +0800

    observer pattern example: WeatherStation and Swing

---
+++ observer/weather/CurrentConditionsDisplay.java
@@ -0,0 +1,24 @@
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private float pressure;
+	private Subject weatherData;
+
+	public CurrentConditionsDisplay(Subject weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		display();
+	}
+
+	public void display() {
+		System.out.println("Current Conditions: " + temperature
+			+ "F degrees and " + humidity + "% humidity and "
+			+ pressure + " pressure.");
+	}
+}
