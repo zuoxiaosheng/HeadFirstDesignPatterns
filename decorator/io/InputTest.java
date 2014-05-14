52e081b44ababdbe818f5690706048e49681e7ab
unknown <Crayon@NBCSRZXS1.cddev.ccb.cn>
2014-05-14 23:56:33 +0800

    decorator pattern examples: starbuzz and io

---
+++ decorator/io/InputTest.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class InputTest {
+
+	public static void main(String[] args) throws IOException {
+		int c;
+		try {
+			InputStream in = new FileInputStream("test.txt");
+			in = new BufferedInputStream(in);
+			in = new LowerCaseInputStream(in);
+
+			while((c = in.read()) >= 0) {
+				System.out.print((char)c);
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
