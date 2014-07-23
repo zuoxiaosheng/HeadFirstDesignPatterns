6cbc943ff8e347c9542a114a1e132848421d5d2d
unknown <Crayon@NBCSRZXS1.cddev.ccb.cn>
2014-07-23 23:06:23 +0800

    composite pattern example: menu

---
+++ composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
